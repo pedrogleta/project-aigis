c64a50ff24b108ed2118ee81c79f701439841c8e
Pedro Leta <pedrogleta@Pedros-MacBook-Pro-2.local>
2025-04-17 19:38:28 -0300

    fix env variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world');
+import 'dotenv/config';
+
+import { App } from '@slack/bolt';
+
+// Initializes your app with your bot token and signing secret
+
+const app = new App({
+  token: process.env.BOT_USER_OAUTH_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  app.logger.info('⚡️ Bolt app is running!');
+})();
